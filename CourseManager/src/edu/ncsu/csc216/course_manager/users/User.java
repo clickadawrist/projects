3deec5ecb0ea749429e7baec6e8cf136dd427cc1
magreen3 <magreen3@ncsu.edu>
2016-05-19 19:35:13 -0400

    making sure the edu/ncsu part is in there...

---
+++ CourseManager/src/edu/ncsu/csc216/course_manager/users/User.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.course_manager.users;
+
+/**
+ * @author kawaii_musume
+ *
+ */
+public abstract class User {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
