61a2a61bce420d9aa189b740f1c04418e3fc5f7a
magreen3 <magreen3@ncsu.edu>
2016-05-19 20:18:15 -0400

    got rid of main method cause instructions told me to

--- CourseManager/src/edu/ncsu/csc216/course_manager/users/User.java
+++ CourseManager/src/edu/ncsu/csc216/course_manager/users/User.java
@@ -9,12 +9,5 @@ package edu.ncsu.csc216.course_manager.users;
  */
 public abstract class User {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
